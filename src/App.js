db28e8197056658a4049d466133cfa934a622230
Viral.pr <viral.p@devhostserver.com>
2023-02-01 19:01:40 +0530

    task3

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import Navbar from './Components/Navbar';
+import Main from '../src/Components/Main';
+import Footer from '../src/Components/Footer';
+import MainFile from './Task-3/MainFile';
+
+function App() {
+  return (
+    <>
+     <Navbar />
+     <Main />
+     <Footer />
+      <MainFile />
+    </>
+  );
+}
+
+export default App;
